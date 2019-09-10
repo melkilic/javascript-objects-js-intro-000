ea7912f5ac0d8d6858b8e4048b71ad5fc4064806
Melike KILIC <melike180400@gmail.com>
2019-09-10 18:53:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, billieEilish, whenThePartyIsOver){
   playlist[billieEilish]= whenThePartyIsOver
   return playlist;
 }
-function removeFromPlaylist(playlist,billieEilish){
-  delete playlist.billieEilish
+function removeFromPlaylist(playlist,lanaDelRey){
+  delete playlist.slowdive
   return playlist;
 }
