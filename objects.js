698e4f9ded07a2d81d166ffa3e4b7c7fa90d9fab
Melike KILIC <melike180400@gmail.com>
2019-09-10 18:11:45 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+
+var playlist{
+  'Lana Del Rey': 'Video Games',
+  'NF':'Lie'
+}
+// var playlist= new Object({lanaDelRey:'video games'});
+// var playlist={lanaDelRey: "blue jeans"};
+function updatePlaylist(playlist,'Billie Eilish','When the party is over'){
+playlist['Billie Eilish']= 'when the party is over'
+ return playlist;
+}
+}
